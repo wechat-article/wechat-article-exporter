f2a5a9bc7d54b283b1e02f3b9d9208c92c6a9dd8
tomczhang <chizhang198908@gmail.com>
2026-04-11 20:36:11 +0800

    feat: 优化PDF的样式

--- types/env.d.ts
+++ types/env.d.ts
@@ -1,3 +1,27 @@
 declare interface Window {
   htmlDocx: any;
 }
+declare module 'html2pdf.js' {
+  interface Html2PdfOptions {
+    margin?: number | number[];
+    filename?: string;
+    image?: { type?: string; quality?: number };
+    html2canvas?: Record<string, any>;
+    jsPDF?: { unit?: string; format?: string; orientation?: string };
+    pagebreak?: Record<string, any>;
+    enableLinks?: boolean;
+  }
+
+  interface Html2PdfWorker {
+    set(options: Html2PdfOptions): Html2PdfWorker;
+    from(element: HTMLElement | string): Html2PdfWorker;
+    save(): Promise<void>;
+    outputPdf(type: 'blob'): Promise<Blob>;
+    outputPdf(type: 'datauristring'): Promise<string>;
+    outputPdf(type?: string): Promise<any>;
+  }
+
+  function html2pdf(): Html2PdfWorker;
+  export default html2pdf;
+}
+
